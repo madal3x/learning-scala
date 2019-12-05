210f2cebeaae90ed51b98f91057cb15c44b2531e
madal3x <alexandru.madan@gmail.com>
2019-12-05 22:40:57 +0100

    funcprog in scala - Monad

--- src/main/scala/funcprog/Monad.scala
+++ src/main/scala/funcprog/Monad.scala
@@ -25,7 +25,67 @@ trait Monad[M[_]] extends Functor[M] {
 
   def traverse[A,B](la: List[A])(f: A => M[B]): M[List[B]] =
     sequence(la.map(f))
+
+  def factor[A,B](ma: M[A], mb: M[B]): M[(A, B)] =
+    map2(ma, mb)((_, _))
+
+  def cofactor[A,B](e: Either[M[A], M[B]]): M[Either[A, B]] = e match {
+    case Left(ma) =>
+      map(ma)(Left(_))
+    case Right(mb) =>
+      map(mb)(Right(_))
+  }
+
+  def compose[A,B,C](f: A => M[B], g: B => M[C]): A => M[C] =
+    a => flatMap(f(a))(g)
+
+  def flatMapFromCompose[A,B](ma: M[A])(f: A => M[B]): M[B] =
+    compose((_: Unit) => ma, f)()
+
+  /** compose(f, unit) == f && compose(unit, f) == f */
+  def idLawCompose[A,B](f: A => M[B]) = (a: A) =>
+    compose(f, (b: B) => unit(b))(a) == f(a) &&
+      compose((_: Unit) => unit(a), f)(a) == f(a)
+
+  /** flatMap(f(a))(unit) == flatMap(unit(a))(f) */
+  def idLawFlatMap[A,B](f: A => M[B]) = (a: A) =>
+    flatMap(f(a))(unit) == flatMap(unit(a))(f)
+
+  def join[A](mma: M[M[A]]): M[A] =
+    flatMap(mma)(ma => ma)
+
+  def flatMapFromJoin[A,B](ma: M[A])(f: A => M[B]): M[B] =
+    join(map(ma)(f))
+
+  def composeFromJoin[A,B,C](f: A => M[B], g: B => M[C]): A => M[C] = (a: A) =>
+    join(map(f(a))(g))
+
+  /** join(unit(unit(a))) == unit(a) */
+  def idLawJoin[A,B] = (a: A) =>
+    join(unit(unit(a))) == unit(a)
 }
+
+object State {
+  case class State[S, A](run: S => (A, S)) {
+    def map[B](f: A => B): State[S, B] = State(s => {
+      val (a, s1) = run(s)
+      (f(a), s1)
+    })
+    def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+      val (a, s1) = run(s)
+      f(a).run(s1)
+    })
+  }
+
+  type IntState[A] = State[Int, A]
+  object intStateMonad extends Monad[IntState] {
+    def unit[A](a: => A): IntState[A] = State(s => (a, s))
+    def flatMap[A, B](ma: IntState[A])(f: A => IntState[B]): IntState[B] =
+      ma.flatMap(f)
+  }
+}
+
+
 
 object Monad {
   val optionMonad = new Monad[Option] {
